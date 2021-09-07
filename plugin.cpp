0af701e7daf42936db7883cae45c73f6ef251346
Ayrton Munoz <adm@immunant.com>
2021-09-06 19:09:34 -0500

    Fix image offset calculation and add image name to output
    This closes #5, though it'd still be good to either patch QEMU to get
    `guest_base` or specify in the README that particular version must be used to
    make sure the `guest_base` is defined.

--- plugin.cpp
+++ plugin.cpp
@@ -22,18 +22,23 @@ static optional<uint64_t> branch_addr = {};
 static ofstream outfile;
 
 typedef struct image_offset {
+    // An offset into a memory-mapped ELF image
     uint64_t offset;
+    // The position of the image name in its /proc/self/maps entry
     size_t image_name_pos;
 } image_offset;
 
-static optional<image_offset> get_offset(const string_view segment, uint64_t addr) {
+// Returns an `image_offset` for the input vaddr if it's in the memory-mapped region defined by the
+// /proc/self/maps entry.
+static optional<image_offset> get_offset(const string_view maps_entry, uint64_t emulator_vaddr) {
     uint32_t name_pos;
     uint64_t from, to, offset;
-    sscanf(segment.data(), "%lx-%lx %*c%*c%*c%*c %lx %*lx:%*lx %*lu %n", &from, &to, &offset,
+    uint64_t real_vaddr = emulator_vaddr + guest_base;
+    sscanf(maps_entry.data(), "%lx-%lx %*c%*c%*c%*c %lx %*lx:%*lx %*lu %n", &from, &to, &offset,
            &name_pos);
-    if ((from <= addr) && (addr <= to)) {
+    if ((from <= real_vaddr) && (real_vaddr <= to)) {
         struct image_offset offset = {
-            .offset = addr + guest_base,
+            .offset = real_vaddr - from,
             .image_name_pos = name_pos,
         };
         return offset;
@@ -47,19 +52,27 @@ static void mark_indirect_branch(uint64_t callsite_vaddr, uint64_t dst_vaddr) {
     string line;
     optional<image_offset> callsite = {};
     optional<image_offset> dst = {};
+    string callsite_image = "";
+    string dst_image = "";
+    // For each entry in /proc/self/maps
     while (getline(maps, line)) {
         if (!callsite.has_value()) {
             callsite = get_offset(line, callsite_vaddr);
             if (callsite.has_value()) {
-                // copy name from line
+                // Copy name since `line` gets reused in this loop
+                char *image_name = line.data() + callsite->image_name_pos;
+                callsite_image = string(image_name);
             }
         }
         if (!dst.has_value()) {
             dst = get_offset(line, dst_vaddr);
             if (dst.has_value()) {
-                // copy name from line
+                // Copy name since `line` gets reused in this loop
+                char *image_name = line.data() + dst->image_name_pos;
+                dst_image = string(image_name);
             }
         }
+        // Skip the remaining entries after finding the callsite and destination
         if (callsite.has_value() && dst.has_value()) {
             break;
         }
@@ -70,14 +83,7 @@ static void mark_indirect_branch(uint64_t callsite_vaddr, uint64_t dst_vaddr) {
     if (!dst.has_value()) {
         cout << "ERROR: Unable to find destination address in /proc/self/maps" << endl;
     }
-    outfile << "0x" << hex << callsite->offset << ",0x" << hex << dst->offset << ",";
-    // if (callsite->image_name) {
-    //    outfile << callsite->image_name;
-    //}
-    // if (dst->image_name) {
-    //    outfile << "," << callsite->image_name;
-    //}
-    outfile << endl;
+    outfile << "0x" << hex << callsite->offset << "," << callsite_image << ",0x" << hex << dst->offset << "," << dst_image << endl;
     return;
 };
 
@@ -97,7 +103,7 @@ static void indirect_branch_exec(unsigned int vcpu_idx, void *callsite_addr) {
     branch_addr = (uint64_t)callsite_addr;
 }
 
-// Callback for indirect branch at the start of a block
+// Callback for indirect branch which may also be the destination of another branch
 static void indirect_branch_at_start(unsigned int vcpu_idx, void *callsite_addr) {
     branch_taken(vcpu_idx, callsite_addr);
     indirect_branch_exec(vcpu_idx, callsite_addr);
@@ -177,7 +183,7 @@ extern int qemu_plugin_install(qemu_plugin_id_t id, const qemu_info_t *info, int
         return -4;
     }
 
-    outfile << "callsite,destination,callsite image,destination image" << endl;
+    outfile << "callsite offset,callsite image,destination offset,destination image" << endl;
     // Register a callback for each time a block is translated
     qemu_plugin_register_vcpu_tb_trans_cb(id, block_trans_handler);
 
--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,11 @@
 echo "Running dynamically linked x86-64 test"
-../qemu/build/qemu-x86_64 -plugin ./libibresolver.so,arg="tests/x86-64/fn_ptr.elf.txt",arg="tests/x86-64/fn_ptr.csv" tests/x86-64/fn_ptr.elf
+../qemu/build/qemu-x86_64 -plugin ./libibresolver.so,arg="tests/x86-64/fn_ptr.csv" tests/x86-64/fn_ptr.elf
 
 echo "Running statically linked x86-64 test"
-../qemu/build/qemu-x86_64 -plugin ./libibresolver.so,arg="tests/x86-64/fn_ptr-static.elf.txt",arg="tests/x86-64/fn_ptr-static.csv" tests/x86-64/fn_ptr-static.elf
+../qemu/build/qemu-x86_64 -plugin ./libibresolver.so,arg="tests/x86-64/fn_ptr-static.csv" tests/x86-64/fn_ptr-static.elf
 
 echo "Running dynamically linked arm32 test"
-../qemu/build/qemu-arm -L /usr/arm-linux-gnueabihf/ -plugin ./libibresolver.so,arg="tests/arm32/fn_ptr.elf.txt",arg="tests/arm32/fn_ptr.csv" tests/arm32/fn_ptr.elf
+../qemu/build/qemu-arm -L /usr/arm-linux-gnueabihf/ -plugin ./libibresolver.so,arg="tests/arm32/fn_ptr.csv" tests/arm32/fn_ptr.elf
 
 echo "Running statically linked arm32 test"
-../qemu/build/qemu-arm -plugin ./libibresolver.so,arg="tests/arm32/fn_ptr-static.elf.txt",arg="tests/arm32/fn_ptr-static.csv" tests/arm32/fn_ptr-static.elf
+../qemu/build/qemu-arm -plugin ./libibresolver.so,arg="tests/arm32/fn_ptr-static.csv" tests/arm32/fn_ptr-static.elf
