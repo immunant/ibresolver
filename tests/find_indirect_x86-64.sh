096ad5506a762a534aa89977dea4005b1ed12593
Ayrton Munoz <adm@immunant.com>
2021-09-08 18:11:21 -0400

    Rerun tests

--- tests/find_indirect_x86-64.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This script should only be used for creating the callsite lists for x86 test binaries
-objdump -d $1 | rg "(jmp |call)   [^0-9,a-f].*$" | awk '{print "0x"$1}' | sed 's/://' > $1.txt
