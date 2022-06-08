812308f2b2511aeb64bccccbe493b6d75eebccbb
Ayrton Munoz <adm@immunant.com>
2022-06-08 15:05:13 -0400

    Make test runner use new QEMU arg parsing

--- run_tests.sh
+++ run_tests.sh
@@ -1,17 +1,17 @@
 echo "Running dynamically linked x86-64 test"
-../qemu/build/qemu-x86_64 -plugin ./libibresolver.so,arg="tests/x86-64/fn_ptr.csv" tests/x86-64/fn_ptr.elf
+../qemu/build/qemu-x86_64 -plugin ./libibresolver.so,output="tests/x86-64/fn_ptr.csv" tests/x86-64/fn_ptr.elf
 
 echo "Running dynamically linked x86-64 test with an offset .text section"
-../qemu/build/qemu-x86_64 -plugin ./libibresolver.so,arg="tests/x86-64/fn_ptr-offset-text.csv" tests/x86-64/fn_ptr-offset-text.elf
+../qemu/build/qemu-x86_64 -plugin ./libibresolver.so,output="tests/x86-64/fn_ptr-offset-text.csv" tests/x86-64/fn_ptr-offset-text.elf
 
 echo "Running statically linked x86-64 test"
-../qemu/build/qemu-x86_64 -plugin ./libibresolver.so,arg="tests/x86-64/fn_ptr-static.csv" tests/x86-64/fn_ptr-static.elf
+../qemu/build/qemu-x86_64 -plugin ./libibresolver.so,output="tests/x86-64/fn_ptr-static.csv" tests/x86-64/fn_ptr-static.elf
 
 echo "Running dynamically linked arm32 test"
-../qemu/build/qemu-arm -L /usr/arm-linux-gnueabihf/ -plugin ./libibresolver.so,arg="tests/arm32/fn_ptr.csv" tests/arm32/fn_ptr.elf
+../qemu/build/qemu-arm -L /usr/arm-linux-gnueabihf/ -plugin ./libibresolver.so,output="tests/arm32/fn_ptr.csv" tests/arm32/fn_ptr.elf
 
 echo "Running dynamically linked arm32 thumb test"
-../qemu/build/qemu-arm -L /usr/arm-linux-gnueabihf/ -plugin ./libibresolver.so,arg="tests/arm32/fn_ptr-thumb.csv" tests/arm32/fn_ptr-thumb.elf
+../qemu/build/qemu-arm -L /usr/arm-linux-gnueabihf/ -plugin ./libibresolver.so,output="tests/arm32/fn_ptr-thumb.csv" tests/arm32/fn_ptr-thumb.elf
 
 echo "Running statically linked arm32 test"
-../qemu/build/qemu-arm -plugin ./libibresolver.so,arg="tests/arm32/fn_ptr-static.csv" tests/arm32/fn_ptr-static.elf
+../qemu/build/qemu-arm -plugin ./libibresolver.so,output="tests/arm32/fn_ptr-static.csv" tests/arm32/fn_ptr-static.elf
